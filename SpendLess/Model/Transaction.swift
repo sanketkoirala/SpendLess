3a5e69bdbd6be3858b0e194047861fea58397ecf
Sanket Koirala <koiralasanket11@gmail.com>
2024-08-20 00:35:37 -0500

    Transaction model

--- SpendLess/Model/Transaction.swift
+++ SpendLess/Model/Transaction.swift
@@ -7,12 +7,29 @@
 
 import SwiftUI
 
-struct Transaction: View {
-    var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+struct Transaction: Identifiable {
+    let id : UUID = .init()
+    var title :  String
+    var remarks :  String
+    var amount :  Double
+    var dateAdded :  Date
+    var category :  String
+    var tintColor :  String
+    
+    init (title: String, remarks: String, amount: Double, dateAdded: Date, category: Category, tintColor: TintColor ) {
+        self.title = title
+        self.remarks = remarks
+        self.amount = amount
+        self.dateAdded = dateAdded
+        self.category = category.rawValue
+        self.tintColor = tintColor.color
     }
+    
+    var color: Color {
+        
+        return tints.first(where: {$0.color == tintColor})?.value ?? appTint
+    }
+    
+    
 }
 
-#Preview {
-    Transaction()
-}
